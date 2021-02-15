5a3b356bfa0597093c820980566a2a1690dcf88f
Colten Appleby <colten96@gmail.com>
2021-02-15 04:32:45 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -17,7 +17,6 @@ def monopoly_with_second_tier
     :rent_in_dollars => {},
     :names => {}
   }
-    
   }
 
 end
@@ -28,6 +27,12 @@ def monopoly_with_third_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
 
+  {:railroads => {
+  :pieces => 4,
+  :rent_in_dollars => {},
+  :names => {}
+  }
+  }
 end
 
 def monopoly_with_fourth_tier
